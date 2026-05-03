7d7753a4efb41ef692f008f5eee4fe52cb3d2234
Creed Petitt <creedpetitt@gmail.com>
2026-05-03 02:39:28 -0500

    create methods to view query information returned from database

--- src/postgres/PgResult.cpp
+++ src/postgres/PgResult.cpp
@@ -2,6 +2,7 @@
 
 #include <libpq-fe.h>
 #include <stdexcept>
+#include <string_view>
 
 PgResult::PgResult(PGresult *res) { res_ = res; }
 
@@ -27,6 +28,45 @@ PgResult::~PgResult() {
     }
 }
 
+int PgResult::rows() const {
+    ensure_result();
+    return PQntuples(res_);
+}
+
+int PgResult::columns() const {
+    ensure_result();
+    return PQnfields(res_);
+}
+
+std::string_view PgResult::column_name(int col) const {
+    ensure_result();
+    const char *name = PQfname(res_, col);
+    if (name == nullptr) {
+        throw std::out_of_range("Column index out of range");
+    }
+    return std::string_view(name);
+}
+
+bool PgResult::is_null(int row, int col) const {
+    ensure_result();
+    check_bounds(row, col);
+    return (PQgetisnull(res_, row, col) == 1);
+}
+
+std::string_view PgResult::value(int row, int col) const {
+    ensure_result();
+    check_bounds(row, col);
+    const char * value = PQgetvalue(res_, row, col);  
+    int len = length(row, col);
+    return std::string_view(value, static_cast<size_t>(len));
+}
+
+int PgResult::length(int row, int col) const {
+    ensure_result();
+    check_bounds(row, col);
+    return PQgetlength(res_, row, col);
+}
+
 ExecStatusType PgResult::getResponseStatus() const {
     if (res_ == nullptr) {
         throw std::runtime_error("PgResult is null");
@@ -37,4 +77,19 @@ ExecStatusType PgResult::getResponseStatus() const {
 
 bool PgResult::is_success(ExecStatusType status) {
     return status == PGRES_TUPLES_OK || status == PGRES_COMMAND_OK;
+}
+
+void PgResult::ensure_result() const {
+    if (res_ == nullptr) {
+        throw std::runtime_error("PgResult is null");
+    }
+}
+
+void PgResult::check_bounds(int row, int col) const {
+    if (row < 0 || row >= rows()) {
+        throw std::out_of_range("Row index out of range");
+    }
+    if (col < 0 || col >= columns()) {
+        throw std::out_of_range("Column index out of range");
+    }
 }
--- src/postgres/PgResult.h
+++ src/postgres/PgResult.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <libpq-fe.h>
+#include <string_view>
 
 class PgResult {
 
   private:
     PGresult *res_ = nullptr;
+    void ensure_result() const;
+    void check_bounds(int row, int col) const;
 
   public:
     explicit PgResult(PGresult *res);
@@ -14,6 +17,13 @@ class PgResult {
     PgResult(PgResult &&other) noexcept;
     PgResult &operator=(PgResult &&other) noexcept;
     ~PgResult();
+
+    int rows () const;
+    int columns() const; 
+    std::string_view column_name(int column) const;
+    bool is_null(int row, int column) const;
+    std::string_view value(int row, int column) const ;
+    int length(int row, int column) const;
 
     ExecStatusType getResponseStatus() const;
     static bool is_success(ExecStatusType status);
