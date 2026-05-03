3d56f220cd9644257d315e024d5a7cd253bfe2cd
Creed Petitt <creedpetitt@gmail.com>
2026-05-02 23:12:17 -0500

    add query methods for query params and prepared statments

--- src/postgres/PgConnection.cpp
+++ src/postgres/PgConnection.cpp
@@ -76,6 +76,77 @@ asio::awaitable<PgResult> PgConnection::query(const std::string &sql) {
     co_return co_await read_results();
 }
 
+asio::awaitable<PgResult>
+PgConnection::query_params(const std::string &sql,
+                           const std::vector<std::string> &params) {
+    co_return co_await query_params(sql, params, {});
+}
+
+asio::awaitable<PgResult>
+PgConnection::query_params(const std::string &sql,
+                           const std::vector<std::string> &params,
+                           const std::vector<Oid> &param_types) {
+                            
+    std::vector<const char *> param_values = str_to_char_pointer(params);
+    if (PQsendQueryParams(conn_, sql.c_str(), static_cast<int>(params.size()),
+                          param_types.empty() ? nullptr : param_types.data(),
+                          param_values.empty() ? nullptr : param_values.data(),
+                          nullptr, nullptr, 0) == 0) {
+        throw std::runtime_error(PQerrorMessage(conn_));
+    }
+
+    co_await flush_outgoing();
+    co_return co_await read_results();
+}
+
+asio::awaitable<PgResult>
+PgConnection::prepare(const std::string &statement_name,
+                      const std::string &sql) {
+    co_return co_await prepare(statement_name, sql, {});
+}
+
+asio::awaitable<PgResult>
+PgConnection::prepare(const std::string &statement_name, const std::string &sql,
+                      const std::vector<Oid> &param_types) {
+    if (PQsendPrepare(conn_, statement_name.c_str(), sql.c_str(),
+                      static_cast<int>(param_types.size()),
+                      param_types.empty() ? nullptr : param_types.data()) ==
+        0) {
+        throw std::runtime_error(PQerrorMessage(conn_));
+    }
+
+    co_await flush_outgoing();
+    co_return co_await read_results();
+}
+
+asio::awaitable<PgResult>
+PgConnection::query_prepared(const std::string &statement_name,
+                             const std::vector<std::string> &params) {
+
+    std::vector<const char *> param_values = str_to_char_pointer(params);
+
+    if (PQsendQueryPrepared(conn_, statement_name.c_str(), static_cast<int>(params.size()),
+                            param_values.empty() ? nullptr
+                                                 : param_values.data(),
+                            nullptr, nullptr, 0) == 0) {
+        throw std::runtime_error(PQerrorMessage(conn_));
+    }
+
+    co_await flush_outgoing();
+    co_return co_await read_results();
+}
+
+std::vector<const char *> PgConnection::str_to_char_pointer(
+    const std::vector<std::string> &string_vector) {
+    std::vector<const char *> char_pointer_vector;
+    char_pointer_vector.reserve(string_vector.size());
+
+    for (const std::string &param : string_vector) {
+        char_pointer_vector.push_back(param.c_str());
+    }
+    return char_pointer_vector;
+}
+
 asio::awaitable<void> PgConnection::connect() {
     while (true) {
         PostgresPollingStatusType status = PQconnectPoll(conn_);
@@ -123,6 +194,7 @@ asio::awaitable<void> PgConnection::flush_outgoing() {
 asio::awaitable<PgResult> PgConnection::read_results() {
     std::optional<PgResult> latest_result;
     bool query_failed = false;
+    std::string error_message;
     while (true) {
         co_await wait_readable();
         // fill buffer with network data from socket
@@ -135,7 +207,7 @@ asio::awaitable<PgResult> PgConnection::read_results() {
             if (result == nullptr) {
 
                 if (query_failed) {
-                    throw std::runtime_error("Postgres query failed");
+                    throw std::runtime_error(error_message);
                 }
 
                 if (!latest_result.has_value()) {
@@ -149,6 +221,7 @@ asio::awaitable<PgResult> PgConnection::read_results() {
             PgResult current_result(result);
             if (!PgResult::is_success(current_result.getResponseStatus())) {
                 query_failed = true;
+                error_message = PQresultErrorMessage(result);
             }
 
             latest_result = std::move(current_result);
--- src/postgres/PgConnection.h
+++ src/postgres/PgConnection.h
@@ -8,17 +8,28 @@
 #include <string>
 
 #include <asio.hpp>
+#include <asio/experimental/parallel_group.hpp>
 
 class PgConnection {
 
   private:
     PGconn *conn_ = nullptr;
     std::unique_ptr<asio::posix::stream_descriptor> socket_;
+
     ConnStatusType getStatus() const;
+
     asio::awaitable<void> flush_outgoing();
+
     asio::awaitable<PgResult> read_results();
+
     asio::awaitable<void> wait_readable();
+
     asio::awaitable<void> wait_writeable();
+
+    asio::awaitable<bool> wait_read_or_write();
+
+    std::vector<const char *>
+    str_to_char_pointer(const std::vector<std::string> &string_vector);
 
   public:
     PgConnection(asio::io_context &ctx, const std::string &conn_string);
@@ -30,5 +41,27 @@ class PgConnection {
 
     bool is_open() const;
     asio::awaitable<PgResult> query(const std::string &sql);
+
+    asio::awaitable<PgResult>
+    query_params(const std::string &sql,
+                 const std::vector<std::string> &params);
+
+    // overloading query_params for optional param_types
+    asio::awaitable<PgResult>
+    query_params(const std::string &sql, const std::vector<std::string> &params,
+                 const std::vector<Oid> &param_types);
+
+    asio::awaitable<PgResult> prepare(const std::string &statement_name,
+                                      const std::string &sql);
+
+    // overloading prepare for optional param_types
+    asio::awaitable<PgResult> prepare(const std::string &statement_name,
+                                      const std::string &sql,
+                                      const std::vector<Oid> &param_types);
+
+    asio::awaitable<PgResult>
+    query_prepared(const std::string &statement_name,
+                   const std::vector<std::string> &params);
+
     asio::awaitable<void> connect();
 };
