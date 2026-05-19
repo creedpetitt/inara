4d274b6e8ea2f8ffbfcde2268203e93295db50ef
Creed Petitt <creedpetitt@gmail.com>
2026-05-18 20:47:07 -0500

    add umbrella header

---
+++ include/orbwvr.h
@@ -0,0 +1,9 @@
+// IWYU pragma: begin_exports
+#pragma once
+
+#include "orbwvr/async.h"
+#include "orbwvr/database.h"
+#include "orbwvr/field.h"
+#include "orbwvr/result.h"
+#include "orbwvr/row.h"
+// IWYU pragma: end_exports
